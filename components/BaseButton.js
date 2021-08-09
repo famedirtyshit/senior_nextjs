73b053f54455cb17f46cd09ffff2f52aa3098aeb
famous <famedirtyshit@gmail.com>
2021-08-09 23:11:34 +0700

    add: BaseButton component

---
+++ components/BaseButton.js
@@ -0,0 +1,40 @@
+export default function Home(prop) {
+
+    const checkFill = () => {
+        if(prop.fill == true){
+            let resultClass = ' py-2 px-5 bg-' + prop.fillColor;     
+            return resultClass;       
+        }else{
+            return "";
+        }
+    }
+
+    const checkRound = () => {
+        if(prop.round == true){
+            let resultClass = ' rounded-' + prop.roundSize;
+            return resultClass;
+        }else{
+            return "";
+        }
+    }
+
+    const checkCustom = () => {
+        if(prop.customClass){
+            return " " + prop.customClass;
+        }else{
+            return "";
+        }
+    }
+
+    const checkTextColor = () => {
+        if(prop.textColor){
+            return " text-" + prop.textColor;
+        }else{
+            return "";
+        }
+    }
+
+    return (
+        <button className={"2xl:text-2xl 2xl:font-normal" + checkFill() + checkRound() + checkTextColor() + checkCustom()}>{prop.value}</button>
+    )
+}
