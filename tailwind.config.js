18ff643856f67fbf6e0a0ddc039bfc482ffbc84a
famous <famedirtyshit@gmail.com>
2021-08-09 23:11:16 +0700

    add: tailwind custom config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,33 @@
 // tailwind.config.js
 module.exports = {
-    purge: [
-      // Use *.tsx if using TypeScript
-      './pages/**/*.js',
-      './components/**/*.js'
-    ]
-    // ...
+  purge: [
+    // Use *.tsx if using TypeScript
+    './pages/**/*.js',
+    './components/**/*.js'
+  ],
+  theme: {
+    screens: {
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1920px',
+      // => @media (min-width: 1920px) { ... }
+    },
+    extend: {
+      colors: {
+        mainGreen: '#356053',
+        mainOrange: '#F0930D'
+      }
+    }
   }
+  // ...
+}
