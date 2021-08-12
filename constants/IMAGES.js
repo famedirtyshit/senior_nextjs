4d3268cb5fcee7af2444384ec8c52da781d705bc
thanachot.ice <thanachot.ice@hotmail.com>
2021-08-12 20:16:21 +0700

    add: LayoutHomePage
    ทำหน้า layout homepage 80%

--- constants/IMAGES.js
+++ constants/IMAGES.js
@@ -1,5 +1,12 @@
 const IMAGES = {
     // Logo 
     catModalHomePage: '/images/catModalHomePage.png',
+    bgDownBt:'/images/bg-downBt.png',
+    downBt:'/images/downBt.png',
+    crop:'/images/crop.png',
+    group:'/images/Group.png',
+    footer:'/images/Footer.png',
+    
+
 };
 export default IMAGES;
--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,18 @@ import BaseButton from '@components/BaseButton'
 import IMAGES from '@constants/IMAGES'
 import ICON from '@constants/ICON'
 import Image from 'next/dist/client/image'
+import HomeStyle from '@styles/Home.module.css'
+import { Typography } from '@material-ui/core'
+
 export default function Home() {
     return (
-        <div className="2xl:container mx-auto">
+        <div className={" mx-auto " + HomeStyle.bgImg}>
             <Head>
                 <title>CatUs</title>
                 <meta name="description" content="CatUs Service" />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <header className="2xl:flex 2xl:flex-wrap 2xl:justify-between 2xl:mx-64 mt-3">
+            <header className="2xl:flex 2xl:flex-wrap 2xl:justify-between 2xl:mx-64 pt-3">
                 <h1 className="2xl:text-5xl 2xl:font-black">Catus</h1>
                 <div className="account-action">
                     <BaseButton value={'Login'}></BaseButton>
@@ -27,7 +30,43 @@ export default function Home() {
                 <section className="cat-img 2xl:absolute 2xl:-top-10 2xl:right-28">
                     <Image src={IMAGES.catModalHomePage} alt='cat' width="878" height="346"/>
                 </section>
-            </main>
+                <section className="down-botton 2xl:flex 2xl:flex-wrap 2xl:justify-end 2xl:mt-40">    
+                    <div className="2xl:mr-28"> 
+                        <Image src={IMAGES.bgDownBt} alt="bgDownBt" width="89" height="89"/>      
+                    </div>
+               </section>
+               <section className="why-catUs-container 2xl:mt-96 2xl:mr-60 2xl:ml-52"> 
+                    <p className="2xl:text-white 2xl:text-4xl 2xl:font-black 2xl:text-center 2xl:mt-32">ทำไมต้องใช้ CatUs</p>  
+                    <section className="2xl:flex 2xl:flex-wrap 2xl:justify-center 2xl:mt-32 ">
+                        <div className="2xl:shadow-lg 2xl:rounded-3xl" style={{backgroundColor:'#FFFFFF',width:"673px",height:"534px"}}>
+                        </div>
+                        <div className="2xl:ml-16">
+                            <p className="2xl:text-white 2xl:text-4xl 2xl:font-black ">What is Lorem Ipsum</p>
+                            <p className="2xl:text-white 2xl:text-4xl font-extralight">Lorem Ipsum is simply dummy <br />text of the printing and  <br />typesetting industry. </p>
+                        </div>
+                    </section>
+                    <section className="2xl:flex 2xl:flex-wrap 2xl:mt-48 2xl:justify-center">      
+                        <div className="2xl:mr-16">
+                            <p className="2xl:text-white 2xl:text-4xl 2xl:font-black">What is Lorem Ipsum</p>
+                            <p className="2xl:text-white 2xl:text-4xl font-extralight">Lorem Ipsum is simply dummy <br />text of the printing and  <br />typesetting industry. </p>
+                        </div>
+                        <div className="2xl:shadow-lg 2xl:rounded-3xl" style={{backgroundColor:'#FFFFFF',width:"673px",height:"534px"}}>
+                        </div>
+                    </section>
+                    <section className="howToUse 2xl:text-center" style={{marginTop:"35rem"}}>
+                        <p className="2xl:text-4xl 2xl:font-medium" >1. Typesetting industrys Lorem Ipsum has been the industry's </p>
+                        <section className="2xl:flex 2xl:flex-wrap 2xl:mt-32 2xl:justify-center ">
+                            <Image src={IMAGES.group} alt="Group" width="70.9" height="338.9" />
+                            <div className="2xl:shadow-lg 2xl:rounded-3xl 2xl:ml-40"  style={{backgroundColor:'#368665',width:"829px",height:"586"}} />
+                        </section>
+                    </section>
+               </section>  
+            </main>  
+            <footer className="2xl:mt-28">
+            <div style={{backgroundColor:'#F4C444',width:"1920px",height:"331px"}}>
+
+            </div>
+            </footer>
         </div>
     )
 }
