bfaaf2531168c696288b07cfda3978c3db33faaa
famedirtyshit <famedirtyshit@gmail.com>
2021-08-16 17:21:11 +0700

    add: image path

--- constants/IMAGES.js
+++ constants/IMAGES.js
@@ -1,5 +1,7 @@
 const IMAGES = {
     // Logo 
     catModalHomePage: '/images/catModalHomePage.png',
+    user: '/images/user.png',
+    defaultImg: '/images/defaultImg.png'
 };
 export default IMAGES;
