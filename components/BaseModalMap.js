a9828d9e737d7c8953ab2ff554ddcd73315be29c
famedirtyshit <famedirtyshit@gmail.com>
2021-08-19 11:30:09 +0700

    change: text placeQuery field container from form tag to div tag

--- components/BaseModalMap.js
+++ components/BaseModalMap.js
@@ -126,9 +126,9 @@ export default function TransitionsModal(prop) {
                     <ThemeProvider theme={theme}>
                         <div id="map-container" className={modalClass.paper}>
                             <div className="2xl:flex flex-wrap 2xl:justify-between">
-                                <form id="searchForm" className={"2xl:flex-grow-0.5 " + inputClass.root} noValidate>
+                                <div className={"2xl:flex-grow-0.5 " + inputClass.root} >
                                     <TextField onInput={queryPlaceHandler} className="2xl:w-3/5" id="placeQuery" label="Search" variant="outlined" />
-                                </form>
+                                </div>
                                 <div className="button-action flex items-center">
                                     {placeQuery.length > 0 ? <Button onClick={searchPlace} variant="contained" color="default" className="2xl:max-h-9" >Search</Button> : <Button variant="contained" color="default" className="2xl:max-h-9" disabled>Search</Button>}
                                     {prop.location != null ? <Button onClick={prop.confirmStatusLocation} variant="contained" color="primary" className={"2xl:max-h-9 " + buttonClass.style}>Confirm</Button> : <Button onClick={prop.confirmStatusLocation} variant="contained" color="primary" className={"2xl:max-h-9 " + buttonClass.style} disabled>Confirm</Button>}
