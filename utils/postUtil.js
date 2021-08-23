fd68ff5f76ab243378a0e066b9fd595355c7d819
famedirtyshit <famedirtyshit@gmail.com>
2021-08-23 19:43:59 +0700

    add: postUtil function

--- utils/postUtil.js
+++ utils/postUtil.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const searchLostCat = {
+const allPostUtil = {
     search: async () => {
         try {
             // if(sexInput==undefined){
@@ -14,7 +14,44 @@ const searchLostCat = {
         } catch (e) {
             return e.response;
         }
+    },
+    post: async (lat, lng, dateInput, sexInput, collarInput, descriptionInput, blob, type) => {
+        let endpoint = '';
+        if (type == 'lost') {
+            endpoint = 'postLostCat'
+        } else {
+            endpoint = 'postFoundCat'
+        }
+        try {
+            let formData = new FormData();
+            formData.append('lat', lat)
+            formData.append('lng', lng)
+            formData.append('date', dateInput)
+            if (sexInput != undefined) {
+                formData.append('sex', sexInput)
+            }
+            if (collarInput != undefined) {
+                formData.append('collar', collarInput)
+            }
+            if (descriptionInput != '') {
+                formData.append('description', descriptionInput)
+            }
+            for (let i = 0; i < blob.length; i++) {
+                formData.append(`fileUp_ ${i + 1}`, blob[i]);
+            }
+            // formData.append('blob',blob)
+            let res = await axios({
+                method: "post",
+                url: process.env.API_KEY + `/${endpoint}/post`,
+                data: formData,
+                headers: { "Content-Type": "multipart/form-data" },
+            })
+            return res;
+        } catch (e) {
+            // console.log(e)
+            return e.response;
+        }
     }
 }
 
-export default searchLostCat;
+export default allPostUtil;
