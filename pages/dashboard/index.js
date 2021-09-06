5fda647117652f04badfe46647e7214c42c4d72f
famedirtyshit <famedirtyshit@gmail.com>
2021-09-06 16:00:19 +0700

    add: transports option in socket connection

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -78,7 +78,7 @@ export default function Dashboard() {
                     let account = await accountUtil.getUser(user.uid);
                     if (account.data.result === true) {
                         // socket
-                        const socket = io(process.env.API_KEY);
+                        const socket = io(process.env.API_KEY,{transports: ['websocket']});
                         socket.on('connect', () => {
                             socket.emit("saveSession", account.data.searchResult[0]._id)
                         })
