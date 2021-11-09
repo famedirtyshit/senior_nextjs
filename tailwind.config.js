f14b44d1a5e6b2d0f2ffcaf1142282ddf341b067
famous <famedirtyshit@gmail.com>
2021-11-09 15:51:15 +0700

    change: tailswind's breakpoint scale

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,16 +7,18 @@ module.exports = {
   // ],
   theme: {
     screens: {
-      'sm': '640px',
+      'ss': '640px',
+      
+      'sm': '768px',
       // => @media (min-width: 640px) { ... }
 
-      'md': '768px',
+      'md': '1024px',
       // => @media (min-width: 768px) { ... }
 
-      'lg': '1024px',
+      'lg': '1280px',
       // => @media (min-width: 1024px) { ... }
 
-      'xl': '1280px',
+      'xl': '1536px',
       // => @media (min-width: 1280px) { ... }
 
       '2xl': '1920px',
