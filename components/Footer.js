e68f625877a2bfaf799a14b66bf7c62204b9fc1d
famous <famedirtyshit@gmail.com>
2021-11-27 15:32:47 +0700

    change: footer layout

--- components/Footer.js
+++ components/Footer.js
@@ -5,26 +5,26 @@ export default function Footer(prop) {
   return (
     <section
       className={cn({
-        'footer-orange flex flex-col justify-center items-center bg-historyBlue': prop.color == 'blue',
-        'footer-orange flex flex-col justify-center items-center bg-mainBgGreen': prop.color != 'blue',
-      })}
-      style={{ width: "100%", height: 'auto' }}
+        'footer-orange flex md:flex flex-col md:flex-col justify-center md:justify-center items-center md:items-center bg-historyBlue': prop.color == 'blue',
+        'footer-orange flex md:flex flex-col md:flex-col justify-center md:justify-center items-center md:items-center bg-mainBgGreen': prop.color != 'blue',
+      }) + ' md:h-64'}
+      style={{ width: "100%" }}
     >
-      <div className="flex flex-wrap lg:gap-52 sm:gap-5 mt-10 ml-5">
+      <div className="flex md:flex flex-wrap md:flex-wrap md:gap-24">
         <div className="text-white">
-          <p className="lg:text-2xl text-xl">ติดต่อเรา</p>
-          <p className="mt-1">+66 6071 2203</p>
-          <p>catusservice@gmail.com</p>
-          <p className="lg:text-5xl text-4xl mt-2 font-black">Catus</p>
-          <p className="text-gray-300 mb-10">© Copyright 2021 CatUs</p>
+          <p className="text-2xl md:text-2xl">ติดต่อเรา</p>
+          <p className="mt-1 md:mt-1">+66 6071 2203</p>
+          <p>catus_helpyou@sit.kmutt.ac.th</p>
+          <p className="text-5xl md:text-5xl mt-4 md:mt-6 font-black md:font-black">Catus</p>
+          <p className="text-gray-300">© Copyright 2021 CatUs</p>
         </div>
         <div className="text-white">
-          <p className="lg:text-2xl text-xl">สถานที่ทำการ</p>
-          <p className="mt-1 -mb-1">
+          <p className="text-2xl md:text-2xl">สถานที่ทำการ</p>
+          <p className="mt-1 md:mt-1 -mb-1 md:-mb-1">
             Space Dragon 168 ซอยประชาอุทิศ 40 ถนนประชาอุทิศ แขวงบางมด
             เขตทุ่งครุ กรุงเทพมหานคร 10140
           </p>
-          <p className="mt-5 lg:mt-20 text-gray-300 mb-10 md:mt-16">
+          <p className="mt-4 md:mt-20 text-gray-300">
             School of Information Technology, King Mongkut&apos;s University
             of Technology Thonburi
           </p>
